dfcf559dc3e7666b2b0f70f4d312e056f157dc16
José Andradez <xjgamx@gmail.com>
2025-04-12 21:33:24 -0400

    feat: add zustand and cart store

---
+++ src/stores/__tests__/cart-store.spec.ts
@@ -0,0 +1,38 @@
+
+import { useCartStore } from "../cart.store";
+import { act, renderHook } from "@testing-library/react";
+
+describe("Cart Store", () => {
+  it("should add an item to the cart", () => {
+    const { result } = renderHook(() => useCartStore());
+    act(() => {
+        result.current.addItem({ id: "1", name: "Test Item", price: 10, genre: "Test", image: "Test", description: "Test", isNew: true });
+    });
+    expect(result.current.items).toEqual([{ id: "1", name: "Test Item", price: 10, genre: "Test", image: "Test", description: "Test", isNew: true }]);
+    expect(result.current.getTotal()).toEqual(10);
+    expect(result.current.isAdded("1")).toBe(true);
+  });
+  it("should remove an item from the cart", () => {
+    const { result } = renderHook(() => useCartStore());
+    act(() => {
+        result.current.addItem({ id: "1", name: "Test Item", price: 10, genre: "Test", image: "Test", description: "Test", isNew: true });
+    });
+    act(() => {
+        result.current.removeItem({ id: "1", name: "Test Item", price: 10, genre: "Test", image: "Test", description: "Test", isNew: true });
+    });
+    expect(result.current.items).toEqual([]);
+    expect(result.current.getTotal()).toEqual(0);
+    expect(result.current.isAdded("1")).toBe(false);
+  });
+  it("should clear the cart", () => {
+    const { result } = renderHook(() => useCartStore());
+    act(() => {
+        result.current.addItem({ id: "1", name: "Test Item", price: 10, genre: "Test", image: "Test", description: "Test", isNew: true });
+    });
+    act(() => {
+        result.current.clearCart();
+    });
+    expect(result.current.items).toEqual([]);
+    expect(result.current.getTotal()).toEqual(0);
+  });
+});
---
+++ src/stores/cart.store.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
+import { Game } from "@/utils/endpoint";
+interface CartState {
+  items: Game[];
+  getTotal: () => number;
+  addItem: (item: Game) => void;
+  removeItem: (item: Game) => void;
+  clearCart: () => void;
+  isAdded: (id: string) => boolean;
+}
+
+const useCartStore = create<CartState>()(
+  persist(
+    devtools((set, get) => ({
+      items: [],
+      getTotal: () => {
+        return get().items.reduce((acc, item) => acc + item.price, 0);
+      },
+      addItem: (item: any) => {
+        set((state: any) => ({
+          items: [...state.items, item],
+        }));
+      },
+      removeItem: (item: any) => {
+        set((state: any) => ({
+          items: state.items.filter((i: any) => i.id !== item.id),
+        }));
+      },
+      clearCart: () => {
+        set({ items: [] });
+      },
+      isAdded: (id: string) => {
+        return get().items.some((item: any) => item.id === id);
+      },
+    })),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+export  {useCartStore}
